cff7129733edb2f4f5426fd7033ed68f1b0579a0
Ariejan de Vroom <ariejan@ariejan.net>
2010-01-18 19:27:23 +0100

    Updated config; Fixed link to sinatra

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,6 +11,24 @@ role :db,  "80.255.251.182", :primary => true
 set :deploy_to, "/var/rails/#{application}"
 set :user, "root"
 
+desc "Hooks to runs after code update"
+task :after_update_code do
+  config_db
+  chown_for_apache
+end
+
+desc "chown for webserver"
+task :chown_for_apache, :roles => :app do
+  run "chown -RL www-data:www-data #{release_path}"
+  run "chown -RL www-data:www-data #{shared_path}"  
+end
+
+desc "Link shared data (like uploaded data) between deployments"
+task :config_db, :roles => :db do
+  run "ln -sf #{shared_path}/failkaart.db #{release_path}/failkaart.db"
+end
+
+
 namespace :deploy do
   task :start do
   end
