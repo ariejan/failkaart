60acbcc9f69d1957edaf7141b97a8e823d9c1c41
Michel de Graaf <michel@re-invention.nl>
2010-01-21 00:33:53 +0100

    improved input form and replaced the twitterbird

--- public/application.js
+++ public/application.js
@@ -30,4 +30,37 @@ function shortenUrl(index) {
 function fullLengthUrl(index) {
   var href = $(this).attr("href");
   $(this).html(href);
+}                      
+
+function switchText() {
+	if (this.innerHTML == $(this).attr('title')) {
+		 this.innerHTML = "";
+		 $(this).removeClass('exampleText');   
+	}   
 }
+$(function() {
+
+	$('textarea[title!=""]').each(function() {           
+		if (this.innerHTML == "") {   
+			this.innerHTML = $(this).attr('title'); 
+			$(this).addClass('exampleText');      
+		}
+	}).focus(switchText).blur(switchText);
+
+
+	$('#fail_text').each(function(){              
+	    max = 80;         
+		left = ""
+		maxed = " karakters over het limiet!"
+		var length = $(this).val().length; 
+		$("#fail_counter").html( length >= max ? ( Math.abs(max - length) + maxed ) :  (max - length) + left);
+		$(this).keyup(function(){
+			var new_length = $(this).val().length;      	   
+			$("#fail_counter").html(new_length >= max ? ( Math.abs(max - new_length) + maxed ) :  (max - new_length) + left);
+		});
+	});    
+	
+});
+
+
+
